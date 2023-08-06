ad50b05123867efd63241e540bfd8ce412247921
Manuel Tinio <mrctinio@gmail.com>
2023-08-07 01:56:02 +0800

    add error class

---
+++ lib/beehiiv/error.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Beehiiv
+  class Error < StandardError
+    attr_reader :status, :status_text, :errors
+
+    def initialize(response)
+      @status = response["status"]
+      @status_text = response["statusText"]
+      @errors = response["errors"]
+      super(response)
+    end
+  end
+end
