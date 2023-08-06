88d2e743dade3eb2284f125e5eb123ad6f06e371
Manuel Tinio <mrctinio@gmail.com>
2023-08-07 01:55:44 +0800

    add beehiiv class

---
+++ lib/beehiiv.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "json"
+require "faraday"
+require "uri"
+require "require_all"
+
+class Beehiiv
+  BASE_URL = URI.parse("https://api.beehiiv.com/")
+
+  def self.client(api_key)
+    Faraday.new(url: BASE_URL) do |conn|
+      conn.request :authorization, "Bearer", -> { api_key }
+    end
+  end
+end
+
+require_relative "./beehiiv/init"
